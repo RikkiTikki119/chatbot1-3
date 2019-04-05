b838239fc0a9a5e5a5f8dcc9a9e2ab17b8065f07
Brandon Serros <47602994+Bserros1020@users.noreply.github.com>
2019-04-05 07:57:02 -0500

    Rename README.md to chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+# chatbot1-3
+def main():
+
+    computerfavcereal= "Honey nut bolts"
+
+    favcereal = input("What is your favorite cereal?: ")
+    print(favcereal + '...is a very nice cereal choice. So...what is your name infidel?')
+    name=input("What is your name??")
+    print('What?!?!?!?!:  ' + name + '...That is my name!!!, I will let you in on a little secrect... my favorite cereal is:')
+    print (computerfavcereal)
+
