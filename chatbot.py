351b4689364a007e6438b3fddc9c2e74f716bbf5
Brandon Serros <47602994+Bserros1020@users.noreply.github.com>
2019-04-09 08:13:56 -0500

    b

--- chatbot.py
+++ chatbot.py
@@ -3,15 +3,13 @@ def morning():
 
     print('Hello this is your computer speaking...we are wondering what our master eats for nutrients?')
 
-#wow 
+#wow
 
     computerfavcereal= "Honey nut bolts"
 
     favcereal = input("What is your favorite cereal?: ")
     print(favcereal + '...is a very nice cereal choice. So...what is your name ol sport?')
-
-=======
-
+    
     name=input("What is your name? ")
 
     print('What?!?!?!?!:  ' + name + ' That is my name!!!, Want to know something funny?...my favorite cereal is:' ,computerfavcereal )
