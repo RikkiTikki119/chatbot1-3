fe755904f7f7415667bacae7fdfca4e769bea803
Ricardo Rosenau <rrosenau0110@myduneland.org>
2019-04-09 08:43:19 -0500

    GREETINGS

--- chatbot.py
+++ chatbot.py
@@ -3,11 +3,14 @@
 
 
 def check_for_greeting(sentence):
+    import random
+    word=input("You greeting:")
     KEYWORDS = ("hello", "hi", "greetings", "yo", "what's up",)
     GREETINGS = ["hello ol' sport", "hey", "*hand shake*", "hey, did you receive my letter?"]
-    sentence=input("You:")
+
     if word.lower() in KEYWORDS:
-        return random.choice(GREETINGS)
+        return(random.choice(GREETINGS))
+
 
 def mornings():
     computerfavcereal= "Honey nut bolts"
