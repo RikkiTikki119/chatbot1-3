a6771dffb172a70db277e714a7be4357b7c49335
Ricardo Rosenau <rrosenau0110@myduneland.org>
2019-04-09 08:08:18 -0500

    5

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,18 @@
 # chatbot1-3
-def Ricardo_part():
+def cerealkiller():
+
     print('Hello this is your computer speaking...we are wondering what our master eats for nutrients?')
 
-    computerfavcereal== "Honey nut bolts"
 
-    favcereal == input("What is your favorite cereal?: ")
-    print(favcereal + '...is a very nice cereal choice. So...what is your name infidel?')
-    name == input()
-    print('What?!?!?!?!:  ' + name + '...That is my name!!!, I will let you in on a little secrect... my favorite cereal is:')
-    print (computerfavcereal)
 
-Ricardo_part():
+    computerfavcereal= "Honey nut bolts"
 
-def Brando_part():
-    print ("brandon is a idiot")
+    favcereal = input("What is your favorite cereal?: ")
+    print(favcereal + '...is a very nice cereal choice. So...what is your name ol sport?')
+        name=input()
+    print('What?!?!?!?!:  ' + name + ' That is my name!!!, Want to know something funny?...my favorite cereal is:' ,computerfavcereal )
+
+cerealkiller()
+input("\n\nPress enter to exit")
+
+def tfunct():
